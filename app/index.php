30b57d3e0e9c49bef81ccc74bfa093fba0a73e57
Yusuke Ando <yandod@gmail.com>
2023-10-07 22:26:23 +0900

    indexを追加

---
+++ app/index.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><a href="step1.php">Step1 (学習データのプロット)</a></li>
+    <li><a href="step2.php">Step2 (最小二乗法による回帰線)</a></li>
+    <li><a href="step3.php">Step3 (勾配降下法による回帰線)</a></li>
+    <li><a href="step4.php">Step4 (ニューラルネットワークによる非線形回帰線)</a></li>
+    <li><a href="step5.php">Step5 (上記に統計情報を追加)</a></li>
+</ul>
